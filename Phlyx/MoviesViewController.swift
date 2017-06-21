9856f009e2cb596edb17ffb65a305bdac69e5edd
Agustin Balquin <abalquin@fb.com>
2017-06-21 14:58:04 -0700

    added README, Added TableView Functionality, Network Request

---
+++ Phlyx/MoviesViewController.swift
@@ -0,0 +1,71 @@
+//
+//  MoviesViewController.swift
+//  Phlyx
+//
+//  Created by Agustin Balquin on 6/21/17.
+//  Copyright © 2017 Agustin Balquin. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies:[[String: Any]] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        cell.titleLabel!.text = title
+        cell.overviewLabel!.text = overview
+        return cell
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        // Do any additional setup after loading the view.
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                
+                // TODO: Get the array of movies
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                // TODO: Store the movies in a property to use elsewhere
+                self.movies = movies
+                // TODO: Reload your table view data
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+
+}
